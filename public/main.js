e6662cefdaed475265327aecc8dc3d6c06dee3e8
doktor_justice <somiandras@gmail.com>
2018-03-01 22:01:53 +0100

    Basic charting on frontend
    Add xhr to retrieve data from server. Add basic charts - to be improved.

--- public/main.js
+++ public/main.js
@@ -1,18 +1,30 @@
 (function(){
   'use strict';
 
-  new Chartist.Line('#chart1', {
-    labels: [1, 2, 3, 4],
-    series: [[100, 120, 180, 200]]
-  });
+  let xhr = new XMLHttpRequest()
+  xhr.addEventListener('load', function() {
+    let data = JSON.parse(this.responseText);
+    
+    let labels = data
+    .map(elem => {
+      let date = new Date(elem.date);
+      let label = date.getHours() + ':' + date.getMinutes();
+      return label;
+    });
 
-  new Chartist.Line('#chart2', {
-    labels: [1, 2, 3, 4],
-    series: [[100, 120, 180, 200]]
-  });
-
-  new Chartist.Line('#chart3', {
-    labels: [1, 2, 3, 4],
-    series: [[100, 120, 180, 200]]
-  });
+    let temps = data.map(elem => elem.temp);
+    let humis = data.map(elem => elem.humi);
+    let luxes = data.map(elem => elem.lux);
+    
+    let dataSeries = [temps, humis, luxes];
+    dataSeries.forEach((s, i) => {
+      let id = '#chart' + (i + 1);
+      new Chartist.Line(id, {
+        labels: labels,
+        series: [s]
+      });
+    });
+  })
+  xhr.open('GET', 'http://localhost:3000/readings');
+  xhr.send();
 })();
